6c7aea427baba629cb37161d30c84db3e8ffbb06
sptaule <lecas83@gmail.com>
2025-02-15 22:00:54 +0100

    Create TypeHF.php

---
+++ TypeHF.php
@@ -0,0 +1,14 @@
+<?php
+
+class TypeHF
+{
+    const int TEXT = 200;
+    const int UNSIGNED_INT = 17;
+    const int SIGNED_INT = 20;
+    const int DATETIME = 135;
+    const int TIME = 134;
+    const int DATE = 133;
+    const int MONEY = 131;
+    const int BOOLEAN = 11;
+    const int FLOAT = 5;
+}
